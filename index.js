6ed1e2edd0e946482ee5da8a0ee791e2cd8ca71b
Eduardo Oliveira <eduardoorx@gmail.com>
2026-06-18 12:22:58 -0300

    Refactor database connection pool initialization

--- index.js
+++ index.js
@@ -6,14 +6,6 @@ import  pg  from "pg";
 const db = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false
-  }
-
-});
-
-const db = new pg.Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
     rejectUnauthorized: false,
   },
 });
